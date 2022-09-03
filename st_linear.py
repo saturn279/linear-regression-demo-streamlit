e0a76c4d92e52d36158fa7b4d7acbb953f7ac681
Sarwesh Karande <45988700+saturn279@users.noreply.github.com>
2022-09-03 17:13:48 +0530

    init

---
+++ st_linear.py
@@ -0,0 +1,46 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+
+
+@st.cache
+def get_data():
+    return pd.read_csv("pizza.csv")
+
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    return pickle.load(open("linear.model", "rb"))
+
+
+lr = get_model()
+
+promote = st.sidebar.slider("Promotion", 0, 100, 50)
+
+y_pred = lr.predict([[promote]])
+y_pred = int(y_pred.round())
+st.title("Output")
+st.subheader(f"Sales: ${y_pred}")
+st.title("Model")
+st.text(f"y[sales]={lr.intercept_.round(2)}+x[promote]({lr.coef_[0].round(2)})")
+st.text(f"{y_pred}={lr.intercept_.round(2)}+{promote}({lr.coef_[0].round(2)})")
+df = get_data()
+
+X = df['Promote'].values[:, np.newaxis]
+y = df['Sales']
+
+fig, ax = plt.subplots()
+df = pd.get_dummies(df)
+ax.scatter(X, y, c="#ADCEFE")
+ax.scatter([[promote]], lr.predict([[promote]]), s=300, c="#4E9F71")
+ax.plot(X, lr.predict(X))
+ax.annotate(f"Predicted Sales \n {promote}, {y_pred}", (promote, y_pred))
+plt.xlabel("Promote")
+plt.ylabel("Sales")
+st.pyplot(fig)
+st.title("Data")
+st.dataframe(df)
+
+
